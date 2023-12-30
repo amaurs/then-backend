82fdaab459a7ec5803d64ea00523ba2c081d3574
amaurs <amaury.gtz@gmail.com>
2023-12-30 10:56:02 -0800

    ⏰ Add point of reference during the same day.

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,36 @@
 extern crate chrono;
 
-use chrono::{DateTime, Utc};
+use chrono::{DateTime, FixedOffset, NaiveDateTime, TimeZone, Utc};
 use lambda_http::{run, service_fn, Body, Error, Request, Response};
 use std::env;
 use serde_json::json;
 
-fn days_elapsed_from_timestamp(timestamp: i64) -> i64 {
-    let timestamp_datetime: DateTime<Utc> = DateTime::from_timestamp(timestamp, 0).unwrap();
-    let current_datetime: DateTime<Utc> = Utc::now();
+fn days_elapsed_from_timestamp(timestamp: i64) -> (bool, i64) {
+    let hour = 3600;
+    let offset = FixedOffset::east_opt(8 * hour).unwrap();
+    let naive = NaiveDateTime::from_timestamp_opt(timestamp, 0).unwrap();
+    let timestamp_datetime: DateTime<FixedOffset> = offset.from_utc_datetime(&naive);
+    let current_datetime_naive: NaiveDateTime = Utc::now().naive_utc();
+    let current_datetime: DateTime<FixedOffset> = offset.from_utc_datetime(&current_datetime_naive);
     let duration = current_datetime.signed_duration_since(timestamp_datetime);
     let days_elapsed = duration.num_days();
 
-    days_elapsed
+    (current_datetime.date_naive() == timestamp_datetime.date_naive() && current_datetime.time() == timestamp_datetime.time(), days_elapsed)
 }
+
+
 
 async fn function_handler(_event: Request) -> Result<Response<Body>, Error> {
     if let Ok(raw_timestamp) = env::var("TIMESTAMP") {
         if let Ok(timestamp) = raw_timestamp.parse::<i64>() {
-            let days_elapsed = days_elapsed_from_timestamp(timestamp);
-            let message = format!("Flora is {} days old today.", days_elapsed);
+            let (is_before, days_elapsed) = days_elapsed_from_timestamp(timestamp);
+
+            let message = if is_before {
+                format!("Flora will be {} days old today.", days_elapsed)
+            } else {
+                format!("Flora is {} days old today.", days_elapsed)
+            };
+
 
             let resp = Response::builder()
                 .status(200)
