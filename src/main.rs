94c60fde1e9febc28d583b034063709299b74a48
amaurs <amaury.gtz@gmail.com>
2024-01-04 09:03:32 -0800

    🪵 Add logging statements.

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,22 @@
 extern crate chrono;
 
 use chrono::{FixedOffset, Utc};
-use lambda_http::{run, service_fn, Body, Error, Request, Response};
+use lambda_http::{run, service_fn, Body, Error, Request, RequestExt, Response};
 use std::env;
 use serde_json::json;
 use then_backend::days_elapsed_between_timestamps;
 
-async fn function_handler(_event: Request) -> Result<Response<Body>, Error> {
+async fn function_handler(request: Request) -> Result<Response<Body>, Error> {
     if let Ok(raw_timestamp) = env::var("TIMESTAMP") {
         if let Ok(timestamp) = raw_timestamp.parse::<i64>() {
+            tracing::info!("raw_http_path: {}", request.raw_http_path());
+            tracing::info!("query_string_parameters: {:#?}", request.query_string_parameters());
+            tracing::info!("query_string_parameters_ref: {:#?}", request.query_string_parameters_ref());
+            tracing::info!("path_parameters: {:#?}", request.path_parameters());
+            tracing::info!("path_parameters_ref: {:#?}", request.path_parameters_ref());
+            tracing::info!("stage_variables: {:#?}", request.stage_variables());
+            tracing::info!("stage_variables_ref: {:#?}", request.stage_variables_ref());
+
             let (is_before, days_elapsed) = days_elapsed_between_timestamps(timestamp, Utc::now().timestamp(), FixedOffset::west_opt(8 * 3600).unwrap());
 
             let message = if is_before {
